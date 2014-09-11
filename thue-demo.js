c050bdda1a00a1fbd5506ac6a92b7ad7e8967b31
Arne Martin Aurlien <am@aurlien.net>
2014-09-11 14:16:22 +0200

    Add thue demo skeleton

---
+++ thue-demo.js
@@ -0,0 +1,23 @@
+var fs = require('fs');
+var shuffle = require('array-shuffle');
+var prompt = require('sync-prompt').prompt;
+
+function parse(str) {
+
+}
+
+function applyRule(state, rule) {
+
+}
+
+function step(program) {
+
+}
+
+function run(program) {
+
+}
+
+var programString = fs.readFileSync(args[0]).toString();
+
+run(parse(programString));
